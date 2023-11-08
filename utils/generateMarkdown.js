66a9976b7bd8edc0cf568ca2b330e59fd45d2352
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-11-07 20:15:04 -0600

    generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,48 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    ![badge] (// insert badge)
+    `;
+  } else {
+    return " ";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}] (// insert link)
+    `;
+  } else {
+    return " ";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    if (license !== 'no license') {
+      return `
+      ## [License] (#table-of-contents)
+      This application uses the following license:
+      ${renderLicenseLink(license)}
+      `;
+    } else {
+      return " ";
+    }
+  }
 }
 
 function generateMarkdown(data) {
   return `
   # ${data.title}
   ${renderLicenseBadge(data.license)}
+
   ## Table-of-Contents
+
   * [Description](#description)
   * [Installation] (#installation)
   * [Usage] (#usage)
-  ${renderLicenseTOC(data.license)}
-  * [Contributing] (#contributing)
-  * [Tests] (#tests)
-  * [Questions] (#questions)
-  ## [Description](#table-of-contents)
 
   ${data.what}
   ${data.why}
   ${data.how}
+
   ## [Installation] (#table-of-contents)
   $(data.installation)
+
   ## [Usage](#table-of-contents)
   ${data.usage}
+
   ${renderLicenseSection(data.license)}
   ## [Contributing] (#table-of-contents)
+
   ${renderContributionSection(data.confirmContributers, data.contribute)}
   ## [Tests] (#table-of-contents)
   ${data.tests}
+  
   ## [Questions] (table-of-contents)
   Please contact me through the following links:
   [GitHub](https://github.com/${data.githubUsername})
