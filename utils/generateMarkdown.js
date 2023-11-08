41034b344e0b9076f29a7694716c97b378875d2e
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-11-07 19:39:10 -0600

    generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,4 +17,37 @@ function generateMarkdown(data) {
 `;
 }
 
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation] (#installation)
+  * [Usage] (#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing] (#contributing)
+  * [Tests] (#tests)
+  * [Questions] (#questions)
+  ## [Description](#table-of-contents)
+
+  ${data.what}
+  ${data.why}
+  ${data.how}
+  ## [Installation] (#table-of-contents)
+  $(data.installation)
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+  ## [Contributing] (#table-of-contents)
+  ${renderContributionSection(data.confirmContributers, data.contribute)}
+  ## [Tests] (#table-of-contents)
+  ${data.tests}
+  ## [Questions] (table-of-contents)
+  Please contact me through the following links:
+  [GitHub](https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})  
+  `;
+}
+
 module.exports = generateMarkdown;
